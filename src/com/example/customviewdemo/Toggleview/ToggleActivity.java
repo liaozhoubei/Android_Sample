47269ed20e5fcc780efa2cf5b1ca2a6fad1bd3e1
liaozhoubei <lzb19890920@sina.com>
2016-07-23 16:04:03 +0800

    finish toggleView

--- src/com/example/customviewdemo/Toggleview/ToggleActivity.java
+++ src/com/example/customviewdemo/Toggleview/ToggleActivity.java
@@ -1,11 +1,12 @@
 package com.example.customviewdemo.Toggleview;
 
 import com.example.customviewdemo.R;
-import com.example.customviewdemo.R.layout;
+import com.example.customviewdemo.Toggleview.ToggleView.OnSwitchStateUpdateListener;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 /**
  * 实现自定义开关按钮的视图
  * @author ASUS-H61M
@@ -19,8 +20,17 @@ public class ToggleActivity extends Activity {
 		setContentView(R.layout.activity_toggleview);
 		ToggleView toggleView = (ToggleView)findViewById(R.id.toggleView);
 		
-		toggleView.setSwitchBackgroundResource(R.drawable.switch_background);
-		toggleView.setSlideButtonResource(R.drawable.slide_button);
-		toggleView.setSwitchState(true);
+//		toggleView.setSwitchBackgroundResource(R.drawable.switch_background);
+//		toggleView.setSlideButtonResource(R.drawable.slide_button);
+//		toggleView.setSwitchState(true);
+		
+		// 设置开关更新监听
+		toggleView.setOnSwitchStateUpdateListener(new OnSwitchStateUpdateListener() {
+			
+			@Override
+			public void onStateUpdate(boolean state) {
+				Toast.makeText(getApplicationContext(), "state: " + state, 0).show();
+			}
+		});
 	}
 }
