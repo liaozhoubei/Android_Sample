ddc89cc3b6e3ec752ba3e9cb7a8a36eab2853bca
liaozhoubei <lzb19890920@sina.com>
2016-07-25 12:04:37 +0800

    finish all

--- src/com/example/customviewdemo/Sliding/SlideMenu.java
+++ src/com/example/customviewdemo/Sliding/SlideMenu.java
@@ -8,13 +8,12 @@ import android.view.ViewGroup;
 import android.widget.Scroller;
 
 public class SlideMenu extends ViewGroup {
-	private View leftMenu;
-	private View mainContent;
-	private float downX;
+	private float downX; // 按下的x坐标
+	private float downY; // 按下的y坐标
 	private float moveX;
-	private int cuttrenState;
-	private int MAIN_CONTENT = 0;
-	private int LEFT_MENU = 1;
+	public static final int MAIN_STATE = 0;
+	public static final int MENU_STATE = 1;
+	private int currentState = MAIN_STATE; // 当前模式
 	private Scroller scroller;
 	
 	public SlideMenu(Context context) {
@@ -33,97 +32,202 @@ public class SlideMenu extends ViewGroup {
 	}
 
 	private void init() {
+		// 初始化滚动器, 数值模拟器
 		scroller = new Scroller(getContext());
 	}
 
-
-
+	/**
+	 * 测量并设置 所有子View的宽高
+	 * widthMeasureSpec: 当前控件的宽度测量规则
+	 * heightMeasureSpec: 当前控件的高度测量规则
+	 */
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		 leftMenu = getChildAt(0);
-		 leftMenu.measure(leftMenu.getLayoutParams().width,
-		 heightMeasureSpec);
 		
-		 mainContent = getChildAt(1);
-		 mainContent.measure(widthMeasureSpec, heightMeasureSpec);
-
+		// 指定左面板的宽高
+		View leftMenu = getChildAt(0);
+		leftMenu.measure(leftMenu.getLayoutParams().width, heightMeasureSpec);
+		
+		// 指定主面板的宽高
+		View mainContent = getChildAt(1);
+		mainContent.measure(widthMeasureSpec, heightMeasureSpec);
+		
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 	}
 
+	/**
+	 * changed: 当前控件的尺寸大小, 位置 是否发生了变化
+	 * left:当前控件 左边距
+	 * top:当前控件 顶边距
+	 * right:当前控件 右边界
+	 * bottom:当前控件 下边界
+	 */
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		 leftMenu = getChildAt(0);
-		 leftMenu.layout(-leftMenu.getMeasuredWidth(), 0, 0, b);
 		
-		 mainContent = getChildAt(1);
-		 mainContent.layout(l, t, r, b);
-
+		// 摆放内容, 左面板
+		View leftMenu = getChildAt(0);
+		leftMenu.layout(-leftMenu.getMeasuredWidth(), 0, 0, b);
+		
+		// 主面板
+		getChildAt(1).layout(l, t, r, b);
 	}
-	
-	 @Override
-	 public boolean onTouchEvent(MotionEvent event) {
-	 switch (event.getAction()) {
-	 case MotionEvent.ACTION_DOWN:
-	 downX = event.getX();
-	 break;
-	
-	 case MotionEvent.ACTION_MOVE:
-	 moveX = event.getX();
-	 int scorllX = (int) (downX - moveX);
-	 int newpositionX = getScrollX() + scorllX;
-	
-	 if (newpositionX < -leftMenu.getMeasuredWidth()){
-	 scrollTo(-leftMenu.getMeasuredWidth(), 0);
-	 } else if (newpositionX > 0) {
-	 scrollTo(0, 0);
-	 } else {
-	 scrollBy(scorllX, 0);
-	 }
-	 downX = moveX;
-	 break;
-	
-	 case MotionEvent.ACTION_UP:
-		 cuttrenState = 0;
-		 int leftCenter = (-leftMenu.getMeasuredWidth() / 2);
-		 if (getScrollX() <= leftCenter){
-			 cuttrenState = LEFT_MENU;
-			 updateCurrentContent();
-		 } else if (getScrollX() > leftCenter){
-			 cuttrenState = MAIN_CONTENT;
-			 updateCurrentContent();
-		 }
-		 
-	 break;
-	
-	 default:
-	 break;
-	 }
-	
-	 return true;
-	 }
 
+	/**
+	 * 处理触摸事件
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX = event.getX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			moveX = event.getX();
+			
+			// 将要发生的偏移量/变化量
+			int scrollX = (int) (downX - moveX);
+			
+			// 计算将要滚动到的位置, 判断是否会超出去, 超出去了.不执行scrollBy
+			// getScrollX() 当前滚动到的位置
+			int newScrollPosition = getScrollX() + scrollX;
+			
+			if (newScrollPosition < -getChildAt(0).getMeasuredWidth()){ // 限定左边界
+				// < -240
+				scrollTo(-getChildAt(0).getMeasuredWidth(), 0);
+			} else if (newScrollPosition > 0){ // 限定右边界
+				// > 0
+				scrollTo(0, 0);
+			} else {
+				// 让变化量生效
+				scrollBy(scrollX, 0);
+			}
+			
+			downX = moveX;
+			
+			break;
+		case MotionEvent.ACTION_UP:
+			// 根据当前滚动到的位置, 和左面板的一半进行比较
+			int leftCenter = (int) (- getChildAt(0).getMeasuredWidth() / 2.0f);
+			
+			if(getScrollX() < leftCenter){
+				// 打开, 切换成菜单面板
+				currentState = MENU_STATE;
+				updateCurrentContent();
+			}else {
+				// 关闭, 切换成主面板
+				currentState = MAIN_STATE;
+				updateCurrentContent();
+			}
+			
+			break;
+		default:
+			break;
+		}
+		
+		return true; // 消费事件
+	}
+
+	/**
+	 * 根据当前的状态, 执行 关闭/开启 的动画
+	 */
 	private void updateCurrentContent() {
 		int startX = getScrollX();
 		int dx = 0;
-		if (cuttrenState == LEFT_MENU){
-//			scrollTo(-leftMenu.getMeasuredWidth(), 0);
-			dx = - leftMenu.getMeasuredWidth() - startX;
-		} else if (cuttrenState == MAIN_CONTENT){
+		// 平滑滚动
+		if(currentState == MENU_STATE){
+			// 打开菜单
+//			scrollTo(-getChildAt(0).getMeasuredWidth(), 0);
+			
+			dx = -getChildAt(0).getMeasuredWidth() - startX;
+			
+		} else {
+			// 恢复主界面
 //			scrollTo(0, 0);
+			
 			dx = 0 - startX;
 		}
-		int duration = Math.abs(dx * 2);
+		
+		// startX: 开始的x值
+		// startY: 开始的y值
+		// dx: 将要发生的水平变化量. 移动的x距离
+		// dy: 将要发生的竖直变化量. 移动的y距离
+		// duration : 数据模拟持续的时长
+		
+		// 1. 开始平滑的数据模拟
+		int duration = Math.abs(dx * 2); 
 		scroller.startScroll(startX, 0, dx, 0, duration);
-		invalidate();
+		
+		invalidate();// 重绘界面 -> drawChild() -> computeScroll();
 	}
 	
+	//2. 维持动画的继续
 	@Override
 	public void computeScroll() {
 		super.computeScroll();
-		if (scroller.computeScrollOffset()) {
-			int currX = scroller.getCurrX();
-			scrollTo(currX, 0);
-			invalidate();
+		if(scroller.computeScrollOffset()){ // 直到duration事件以后, 结束
+			// true, 动画还没有结束
+			// 获取当前模拟的数据, 也就是要滚动到的位置
+			int currX = scroller.getCurrX(); 
+			System.out.println("currX: " + currX);
+			scrollTo(currX, 0); // 滚过去
+			
+			invalidate(); // 重绘界面-> drawChild() -> computeScroll();循环
 		}
+	}
+	// 打开左侧菜单
+	public void open(){
+		currentState = MENU_STATE;
+		updateCurrentContent();
+	}
+	// 关闭左侧菜单
+	public void close(){
+		currentState = MAIN_STATE;
+		updateCurrentContent();
+	}
+	
+	public void switchState(){
+		if(currentState == MAIN_STATE){
+			open();
+		}else {
+			close();
+		}
+	}
+	// 获得现在的开关状态
+	public int getCurrentState(){
+		return currentState;
+	}
+	
+	
+	/**
+	 * 拦截判断
+	 */
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX = ev.getX();
+			downY = ev.getY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			
+			float xOffset = Math.abs(ev.getX() - downX);
+			float yOffset = Math.abs(ev.getY() - downY);
+			
+			if(xOffset > yOffset && xOffset > 5){ // 水平方向超出一定距离时,才拦截
+				return true; // 拦截此次触摸事件, 界面的滚动
+			}
+			
+			break;
+		case MotionEvent.ACTION_UP:
+			
+			break;
+
+		default:
+			break;
+		}
+		return super.onInterceptTouchEvent(ev);
 	}
 }
--- src/com/example/customviewdemo/Sliding/SlidingActivity.java
+++ src/com/example/customviewdemo/Sliding/SlidingActivity.java
@@ -1,21 +1,35 @@
 package com.example.customviewdemo.Sliding;
 
 import com.example.customviewdemo.R;
-import com.example.customviewdemo.R.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 /**
  * 实现策划面板的视图
  * @author ASUS-H61M
  *
  */
-public class SlidingActivity extends Activity{
+public class SlidingActivity extends Activity implements OnClickListener{
+	private SlideMenu sm;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_sliding);
+		sm = (SlideMenu) findViewById(R.id.sm);
+		findViewById(R.id.ib_back).setOnClickListener(this);
+	}
+
+    public void onTabClick(View view){
+    	
+    }
+	
+	@Override
+	public void onClick(View v) {
+		sm.switchState();
 	}
 }
