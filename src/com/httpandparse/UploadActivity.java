ab5abad5cd791f7facbf04fb217b2777de35fd66
liaozhoubei <lzb19890920@sina.com>
2016-08-27 16:19:59 +0800

    chang something

--- src/com/httpandparse/UploadActivity.java
+++ src/com/httpandparse/UploadActivity.java
@@ -30,20 +30,43 @@ public class UploadActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 							  //http://192.168.2.124:8080/bei/servlet/UploaderServlet
-				String url = "http://192.168.2.124:8080/bei/servlet/UploaderServlet";
+				String url = "http://192.168.2.124:8080/Upload/Upload";
 				File file = Environment.getExternalStorageDirectory();
-				File fileAbs = new File(file, "Sky.jpg");
-				if (fileAbs.exists()){
+				
+				File downloadfile = new File(getFilePath());
+				
+				
+				if (downloadfile.exists()){
 					System.out.println("存在这个文件");
 				} else {
 					System.out.println("不存在Sky");
 				}
-				String fileName = fileAbs.getAbsolutePath();
+				String fileName = downloadfile.getAbsolutePath();
 				System.out.println(fileName);
 				UploadThread thread = new UploadThread(url, fileName);
 				thread.start();
 			}
 		});
+	}
+	
+	private static String getFilePath() {
+		// 从下载的URL地址中截取文件名
+		String fileName = "3974346_1426551981202_mthumb.jpg";
+		StringBuffer sb = new StringBuffer();
+		// 获取SD卡根目录的绝对路径
+		String sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+		sb.append(sdcardPath);
+		sb.append(File.separator);
+		sb.append("HttpAndParse");
+		sb.append(File.separator);
+		sb.append("download");
+
+		File dirFile = new File(sb.toString());
+		if (!dirFile.exists() || !dirFile.isDirectory()) {
+			// 如果文件夹不存在,或者此文件不是一个文件夹,需要创建文件夹
+			dirFile.mkdirs();
+		}
+		return sb.toString() + File.separator + fileName;
 	}
 
 }
